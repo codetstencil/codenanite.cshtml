e38d561b0e1e44d459495a06aa326b3d1f6787f8
Ayo Dahunsi <ayo@dolasoft.com>
2021-01-31 05:15:21 -0700

    Updated Expansion Nuget package

--- src/Cshtml/Properties/AssemblyInfo.cs
+++ src/Cshtml/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]  
 [assembly: AssemblyCompany("ZeraSystems Inc.")]  
 [assembly: AssemblyProduct("CodeStencil Code Nanites")]  
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]  
 [assembly: AssemblyCulture("")]  
   
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers   
 // by using the '*' as shown below:  
 // [assembly: AssemblyVersion("1.0.*")]  
-[assembly: AssemblyVersion("1.3.2")]  
-[assembly: AssemblyFileVersion("1.3.2")]  
+[assembly: AssemblyVersion("1.3.3")]  
+[assembly: AssemblyFileVersion("1.3.3")]  
 
